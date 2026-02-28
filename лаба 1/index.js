9e0df4631127a37f4d935b59f6053b644ae4c381
liza <lizaletvinenko331@gmail.com>
2026-02-28 21:43:25 +0200

    first commit

---
+++ лаба 1/index.js
@@ -0,0 +1,6 @@
+import { bookGenerator, readBooksWithTimeout } from './generators.js';
+
+const myLibrary = ["Одного разу розбите серце","Гаррі Поттер","Відьмак","Маленький принц","Завжди і до віку","Балада про недовго й нещасливо","Прокляття справжнього кохання","Безсила","Безросудна","Безсердечний мисливець"];
+const promoSteam = bookGenerator(myLibrary);
+// Запускаємо на 5 секунд 
+readBooksWithTimeout(promoSteam,10);
