a3887ebf3af70725709d95661906da101865a86e
liza <lizaletvinenko331@gmail.com>
2026-04-06 15:29:00 +0300

    Lab4: implement enqueue operation

--- generator-library/library/priorityQueue.js
+++ generator-library/library/priorityQueue.js
@@ -3,5 +3,12 @@ export class BiDirectionalPriorityQueue {
     constructor() {
         this.items = [];
     }
-
+    
+    enqueue(item,priority){
+        const element = {
+            item: item,
+            priority: priority
+        };
+        this.items.push(element);
+    }
 }
