1bec39a04538e3322c2c680138421f6ab7af3021
liza <lizaletvinenko331@gmail.com>
2026-04-11 20:25:50 +0300

    Lab4: add peek operations with optimized seach

--- generator-library/library/priorityQueue.js
+++ generator-library/library/priorityQueue.js
@@ -6,9 +6,43 @@ export class BiDirectionalPriorityQueue {
     
     enqueue(item,priority){
         const element = {
-            item: item,
-            priority: priority
+            item,
+            priority,
         };
         this.items.push(element);
+    }
+    peek(type){
+        if(this.items.length === 0){
+            return null;
+        }
+        if(type === "oldest"){
+            return this.items[0];
+        }
+        if (type === "newest"){
+            return this.items[this.items.length -1];
+        }
+        if (type === "highest"){
+            let max = this.items[0];
+
+            for (let i=1; i< this.items.length; i++){
+                if (this.items[i].priority>max.priority){
+                    max = this.items[i];
+                }
+            }
+
+            return max;
+        }
+        if (type === "lowest"){
+            let min = this.items[0];
+
+            for (let i =1; i< this.items.length; i++){
+                if (this.items[i].priority<min.priority){
+                    min = this.items[i];
+                }
+            }
+            return min;
+          
+        }
+        return null;
     }
 }
