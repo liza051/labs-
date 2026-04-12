44ad05d0006e70231b038626c0b606ad4ea34f35
liza <lizaletvinenko331@gmail.com>
2026-04-12 22:25:48 +0300

    Lab4: implement dequeue operations

--- generator-library/library/priorityQueue.js
+++ generator-library/library/priorityQueue.js
@@ -45,4 +45,34 @@ export class BiDirectionalPriorityQueue {
         }
         return null;
     }
+    dequeue(type) {
+        if (this.items.length === 0){
+            return null;
+        }
+        if (type === "oldest"){
+            return this.items.shift();
+        }
+        if (type === "newest"){
+            return this.items.pop();
+        }
+        if (type === "highest"){
+            let index = 0 ;
+            for(let i=1; i< this.items.length;i++){
+                if(this.items[i].priority > this.items[index].priority){
+                    index = i;
+                }
+            }
+            return this.items.splice(index,1)[0];
+        }
+        if (type === "lowest"){
+            let index = 0;
+            for (let i = 1; i< this.items.length; i++) {
+                if (this.items[i].priority < this.items[index].priority){
+                    index = i;
+                }
+            }
+            return this.items.splice(index,1)[0];
+        }
+        return null;
+    }
 }
