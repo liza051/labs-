a2be40d1b9cd67826142d43541a98c44848aa4ed
liza <lizaletvinenko331@gmail.com>
2026-04-21 13:24:00 +0300

    Lab 5: improve callback handling and add promise version

--- generator-library/library/asyncMap.js
+++ generator-library/library/asyncMap.js
@@ -2,19 +2,44 @@ export function asyncMapCallback(array, asyncCallback, done) {
 
     const result = [];
     let completed = 0;
+    let hasError = false;
 
     array.forEach((item, index) => {
 
-        asyncCallback(item, (value) => {
+        asyncCallback(item, (err, value) => {
+
+            if (hasError) {
+                return;
+            }
+
+            if (err) {
+
+                hasError = true;
+
+                return done(err, null);
+            }
 
             result[index] = value;
 
             completed++;
 
             if (completed === array.length) {
-                done(result);
+
+                done(null, result);
             }
         });
 
     });
+}
+
+export function asyncMapPromise(array, asyncFunction) {
+
+    return Promise.all(
+
+        array.map(async (item) => {
+
+            return await asyncFunction(item);
+
+        })
+    );
 }
