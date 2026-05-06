bad8422b045f02edd7fe62608564033d920d8e2e
liza <lizaletvinenko331@gmail.com>
2026-05-06 17:17:00 +0300

    Lab 6

--- generator-library/library/dataStream.js
+++ generator-library/library/dataStream.js
@@ -6,6 +6,26 @@ export async function* dataStream(limit) {
             setTimeout(resolve,100)
         );
 
+        if (i === 7) {
+
+            throw new Error("Stream processing error");
+        }
+
         yield i;
+    }
+}
+
+export async function processStream(stream) {
+
+    try {
+
+        for await (const item of stream) {
+
+            console.log("Processing:", item);
+        }
+
+    } catch (error) {
+
+        throw error;
     }
 }
